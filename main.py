f1070ec967d4ad0ddf2e842f2650092b0455fb7d
DevTeymur <teymurr.601@gmail.com>
2025-04-30 11:28:02 +0200

    csv format data of the mat file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import scipy.io
+import pandas as pd
 
 mat = scipy.io.loadmat('Xtrain.mat')
+# print(mat['Xtrain'].shape)
 
-print(mat)
+df = pd.DataFrame(mat['Xtrain'])
+print(df.head())
+print(df.shape)
+
+df.to_csv('Xtrain.csv', index=False)
