da22d0dac0c9dd2d036a51f51c2685036476aa95
Max Verweij <m.t.verweij@students.uu.nl>
2025-05-27 19:57:13 +0200

    initial commit for as 2

--- Assignment 2/parse_data.py
+++ Assignment 2/parse_data.py
@@ -6,6 +6,7 @@ def get_dataset_name(filename_with_dir):
     filename_without_dir = filename_with_dir.split('/')[-1]
     temp = filename_without_dir.split('_')[:-1]
     dataset_name = '_'.join(temp)
+    print('dataset_name:',dataset_name)
     return dataset_name
 
 def extract_label(filename):
@@ -15,31 +16,18 @@ def extract_label(filename):
         return 'motor'
     elif 'memory' in filename:
         return 'memory'
-    else:
+    elif 'math' in filename:
         return 'math'
+    else:
+        raise ValueError("No valid y label in filename")
 
-def load_all_data(folder_path, logs=False):
+def load_all_data(folder_path, logs=True):
     X = []
     y = []
 
     for filename in os.listdir(folder_path):
         if filename.endswith('.h5'):
-            print(f"Loading {filename}...") if logs else None
             file_path = os.path.join(folder_path, filename)
-            with h5py.File(file_path, 'r') as f:
-                dataset_name = get_dataset_name(file_path)
-                data = f.get(dataset_name)[()]
-                label = extract_label(filename)
-                print(f"Data shape: {data.shape}, label: {label}") if logs else None
-                X.append(data)       # shape (248, 35624)
-                y.append(label)      # 'motor', 'rest', etc.
-    return X, y
+ 
+    return np.array(X), np.array(y)
 
-
-if __name__ == "__main__":
-    # Example usage
-    # Specify the folder name and load the data. (Don't push data folders to GitHub!!!)
-    X_train, y_train = load_all_data("Intra/train", logs=1)
-    print(f"X_train shape: {np.array(X_train).shape}") #3D array: (n_samples, n_channels, n_times)
-    print(f"y_train shape: {np.array(y_train).shape}") #1D array: (n_samples,)
-    print(y_train)
--- Assignment 2/preprocess_data.py
+++ Assignment 2/preprocess_data.py
@@ -2,6 +2,7 @@ from parse_data import load_all_data
 
 import numpy as np
 
+
 def z_score_normalize(data):
     # data: shape (248, T)
     mean = np.mean(data, axis=1, keepdims=True)
@@ -12,10 +13,3 @@ def z_score_normalize(data):
 def downsample(data, step=8):
     # data: shape (248, T)
     return data[:, ::step]  # keep every 8th column
-
-
-X_train, y_train = load_all_data("Intra/train", logs=0)
-print(f"X_train shape before preprocessing {np.array(X_train).shape}")  # 3D array: (n_samples, n_channels, n_times)
-
-X_train = [downsample(z_score_normalize(x), step=8) for x in X_train]
-print(f"X_train shape after preprocessing {np.array(X_train).shape}")  # 3D array: (n_samples, n_channels, n_times)
